c57120ac0d9dc8f82508f581224f72474fff069c
Anklesh Vishwakarama <knowngate@users.noreply.github.com>
2017-01-10 01:57:42 +0530

    -amd64

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,5 @@
 WickedPdf.config = {
-    :wkhtmltopdf => '/opt/wkhtmltopdf',
+    :wkhtmltopdf => '/opt/wkhtmltopdf-amd64',
     :layout => "pdf.html",
     :margin => {    :top=> 40,
                     :bottom => 20,
